cb0d1e2ac1b31d35bfc09f8b48a77cda2a1e92b9
rakesh mekala <rakeshyadavmekala15@gmail.com>
2024-10-08 12:42:34 +0530

    ci

--- src/main/java/com/neoteric/docker/DockerController.java
+++ src/main/java/com/neoteric/docker/DockerController.java
@@ -9,7 +9,7 @@ public class DockerController {
 
     @GetMapping(value = "/api/docker")
     public String getHelloWorld(){
-        return "Hello World";
+        return "Hello World jenkins";
     }
 
 }
